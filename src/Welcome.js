e2055f656264f1bd92802b63c4298a1f2c7df302
jinboqwer <875017308@qq.com>
2018-11-21 08:35:06 +0800

    first commit

---
+++ src/Welcome.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class Welcome extends React.Component {
+
+    render() {
+        const todoList = ["lean react","lean weixin"],
+              isLogin = true,
+              test = <div>React</div>;
+              console.log(test)
+        return (
+            <div className="this">
+                <h1>Hello World</h1>
+                {isLogin ? <p>欢迎</p> : <p>请登录</p>}
+                <ul>
+                    {todoList.map(item => <li>{item}</li>)}
+                </ul>
+            </div> 
+        )  
+    }
+}
+
+export default Welcome

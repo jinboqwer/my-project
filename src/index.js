acec749e685e6c0058d2875a652a21a1917851e3
jinboqwer <875017308@qq.com>
2018-12-01 16:11:20 +0800

    学习redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 //import Welcome from './Welcome';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 //import NameCard from './Components/NameCard';
-import CssTest from './Components/CssTest.js';
+//import CssTest from './Components/CssTest.js';
+import TodoList from './Components/TodoList';
 
-ReactDOM.render(<CssTest />, document.getElementById('root'));
+ReactDOM.render(<TodoList />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux'
+import Reducers from './Reducer'
+
+const store = createStore(Reducers) 
+
+export default store;
+
+
